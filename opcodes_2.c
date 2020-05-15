9139ee81c468131ef6fde3cde1d96f9d2814d8aa
Camilo Moncada <monk-da@hotmail.com>
2020-05-14 23:59:09 -0500

    response questions

---
+++ opcodes_2.c
@@ -0,0 +1,75 @@
+#include "monty.h"
+/**
+ * pint - prints the value at the top of stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void pint(stack_t **stack, unsigned int nline)
+{
+	stack_t *temp;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", nline);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = *stack;
+	while (temp)
+	{
+		if (temp->prev == NULL)
+			break;
+		temp = temp->prev;
+	}
+
+	printf("%d\n", temp->n);
+}
+
+/**
+ * pop - removes the top element of stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void pop(stack_t **stack, unsigned int nline)
+{
+	if (stack == NULL || *stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", nline);
+		exit(EXIT_FAILURE);
+	}
+	/* if stack is more than 1 node, else free entire thing */
+	if ((*stack)->next != NULL)
+	{
+		*stack = (*stack)->next;
+		free((*stack)->prev);
+		(*stack)->prev = NULL;
+	}
+	else
+	{
+		free(*stack);
+		*stack = NULL;
+	}
+}
+
+/**
+ * swap - swaps the top two elements of the stack
+ * @stack: pointer to the head node pointer of stack
+ * @nline: the line number
+ * Return: Nothing.
+ */
+void swap(stack_t **stack, unsigned int nline)
+{
+	int temp;
+
+	if (stack == NULL || *stack == NULL || !((*stack)->next))
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", nline);
+		exit(EXIT_FAILURE);
+	}
+
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
