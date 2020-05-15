2033927d2a77f79b4c789b90ac0452d38232e217
Camilo Moncada <monk-da@hotmail.com>
2020-05-14 21:02:50 -0500

    response question 0

---
+++ get_content.c
@@ -0,0 +1,69 @@
+#include "monty.h"
+
+/**
+ * format_line - get lines the file
+ * @line: text content the file
+ * @buffer: content text no format the file
+ * Return: void
+ */
+
+void format_line(char *buffer, line_t *line)
+{
+	char *token = NULL;
+	int i;
+
+	token = strtok(buffer, " '\n'");
+
+	line->content = malloc(sizeof(char *) * strlen(token));
+
+	if (!line->content)
+	{
+		fprintf(stderr, "Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
+
+	line->number = 0;
+
+	for (i = 0; token && i < 2; i++)
+	{
+		line->content[i] = token;
+		line->number++;
+		token = strtok(NULL, " \n");
+	}
+
+	line->content[i] = NULL;
+
+}
+
+/**
+ * get_content - get lines the file
+ * @file: file for get content
+ *
+ * Return: void
+ */
+
+void get_content(FILE *file)
+{
+	char *buffer = NULL;
+	store_t *sotre_st = NULL;
+	size_t l = 0;
+	ssize_t read = 0;
+	line_t line;
+
+	buffer = malloc(sizeof(char *) * l);
+
+	if (!buffer)
+	{
+		fprintf(stderr, "Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
+
+	while ((read = getline(&buffer, &l, file)) != -1)
+	{
+		format_line(buffer, &line);
+		get_op_func(line, file)(&(sotre_st->stack), line.number);
+	}
+
+	free(buffer);
+	fclose(file);
+}
---
+++ monty.h
@@ -0,0 +1,82 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <signal.h>
+
+extern int argint;
+/**
+ * struct store_s - doubly linked list representation of a stack (or queue)
+ * @stack: content linked list
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct store_s
+{
+	stack_t *stack;
+} store_t;
+
+/**
+ * struct f_line - contents of line and corresponding number
+ * @content: array of tokens read from the line
+ * @number: the line number
+ *
+ * Description: contents of a line and corresponding number
+ */
+typedef struct f_line
+{
+	unsigned int number;
+	char **content;
+} line_t;
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+void get_content(FILE *file);
+void format_line(char *buffer, line_t *line);
+
+void push(stack_t **stack, unsigned int nline);
+void pall(stack_t **stack, unsigned int nline);
+void free_stack(stack_t **stack);
+
+
+void (*get_op_func(line_t line, FILE *file))(stack_t **, unsigned int);
+
+bool opcode_check(line_t line);
+bool argument_check(char *token);
+void push_check(line_t line, char *opcode, FILE *file);
+
+#endif
